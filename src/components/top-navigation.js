cb5cdd9c2cc2934c02bcb5d019c3187853fe095e
nathan <nathan@cubed.email>
2020-07-17 23:02:51 +0100

    add click handler to hamburger icon.

--- src/components/top-navigation.js
+++ src/components/top-navigation.js
@@ -1,10 +1,21 @@
 import React, { Component } from "react";
 
 class TopNavigation extends Component {
+    constructor(props){
+        super(props)
+        
+        this.onHamburgerClick = this.onHamburgerClick.bind(this);
+    }
+
+    onHamburgerClick(event){
+        // Call parent click handler and pass the event.
+        this.props.onClick(event)
+    }
+    
     render(){
         return(
             <div className="top-nav">
-                <div className="top-nav__hamburger">
+                <div onClick={this.onHamburgerClick} className="top-nav__hamburger">
                      <i class="fas fa-bars"></i>
                 </div>
             </div>
