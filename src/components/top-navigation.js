b896441ad2e540548a436ad21986f9af7a35a753
nathan <nathan@cubed.email>
2020-07-17 21:54:13 +0100

    add file top-navigation.js

---
+++ src/components/top-navigation.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class TopNavigation extends Component {
+    render(){
+        return(
+            <div className="top-nav">
+                <h1>mr top nav</h1>
+            </div>
+        )
+    }
+}
+
+export default TopNavigation;
