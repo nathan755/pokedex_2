575ced16a43284f9e605c1d3e98a43dd987390c5
nathan <nathan@cubed.email>
2020-07-17 21:37:09 +0100

    initial project setup

---
+++ src/components/home.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+    constructor(props){
+        super(props)
+
+    }
+    
+
+    render(){
+        return(
+            <div>
+                <h1>Hello Home</h1>
+            </div>
+        )
+    }
+}
+
+export default Home;
