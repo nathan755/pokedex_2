bbccfb2d127f80b375f99269309caa942c2c63fe
nathan <nathan@cubed.email>
2020-07-18 21:42:38 +0100

    add side nav toggle function

--- src/components/home.js
+++ src/components/home.js
@@ -2,18 +2,33 @@
 import React, { Component } from "react";
 // Components
 import TopNav from "./top-navigation";
+import SideNav from "./side-nav";
 
 class Home extends Component {
     constructor(props){
         super(props)
 
+        this.state = {
+            sideNavIsOpen:false
+        }
+
+        this.sideNavToggler = this.sideNavToggler.bind(this);
+
     }
+
+    sideNavToggler(){
+        // Handle opening and closing side navigation
+        this.setState({sideNavIsOpen:!this.state.sideNavIsOpen});
+        
+    }
+
     
 
     render(){
         return(
             <div>
-                <TopNav />
+                <TopNav sideNavIsOpen={this.state.sideNavIsOpen} onClick={this.sideNavToggler} />
+                <SideNav sideNavIsOpen={this.state.sideNavIsOpen} />
             </div>
         )
     }
