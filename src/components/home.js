572763e46f3947da18cc5d7d4faa7d0bd64ab824
nathan <nathan@cubed.email>
2020-07-18 23:45:15 +0100

    add search bar component to home

--- src/components/home.js
+++ src/components/home.js
@@ -3,6 +3,7 @@ import React, { Component } from "react";
 // Components
 import TopNav from "./top-navigation";
 import SideNav from "./side-nav";
+import SearchBar from "./search-bar";
 
 class Home extends Component {
     constructor(props){
@@ -29,6 +30,7 @@ class Home extends Component {
             <div>
                 <TopNav sideNavIsOpen={this.state.sideNavIsOpen} onClick={this.sideNavToggler} />
                 <SideNav sideNavIsOpen={this.state.sideNavIsOpen} />
+                <SearchBar />
             </div>
         )
     }
