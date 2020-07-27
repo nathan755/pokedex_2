60c5fc11f37683f4a400a4e74338ae9e5fb2c609
nathan <nathan@cubed.email>
2020-07-27 20:53:56 +0100

    inital stats pulled

--- src/components/PokemonStatView.js
+++ src/components/PokemonStatView.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Axios from "axios";
+import Stats from "./stats";
 
 /**Overall pokemon stats component. Component gets data and passes it down to smaller components */
 class PokemonStatView extends Component{
@@ -16,8 +17,32 @@ class PokemonStatView extends Component{
             name:"",
             order:"",
             stats:[],
-            weight:""
+            weight:"",
+            types:[]
         }
+
+        this.pokeColours = {
+            bug: "A8B820",
+            dark: "705848",
+            dragon: "7038F8",
+            electric: "F8D030",
+            fairy: "EE99AC",
+            fighting: "C03028",
+            fire: "F08030",
+            flying: "A890F0",
+            ghost: "705898",
+            grass: "78C850",
+            ground: "E0C068",
+            ice: "98D8D8",
+            normal: "A8A878",
+            poison: "A040A0",
+            psychic: "F85888",
+            rock: "B8A038",
+            steel: "B8B8D0",
+            water: "6890F0"
+          };
+
+        this.renderBackgroundDivs = this.renderBackgroundDivs.bind(this);
 
     }
 
@@ -34,19 +59,50 @@ class PokemonStatView extends Component{
                 moves:response.data.moves,
                 weight:response.data.weight,
                 height:response.data.height,
-                image:response.data.sprites.back_default,
-                abilities:response.data.abilities
+                image:response.data.sprites.front_default,
+                abilities:response.data.abilities,
+                types:response.data.types
             })
         })
+    }
+
+    renderBackgroundDivs(){
+        
+        if(this.state.types.length === 2){
+            
+            return(
+                <>
+                    <div style={{backgroundColor: "#"+this.pokeColours[this.state.types[0].type.name]}} className="pokemon-stat-view__left"></div>
+                    <div style={{backgroundColor: "#"+this.pokeColours[this.state.types[1].type.name]}} className="pokemon-stat-view__right"></div>
+                </>
+            )
+        }
+        else if (this.state.types.length === 1){
+            // Check in place because on first render types doesnt exist yet
+            return <div style={{backgroundColor: "#"+this.pokeColours[this.state.types[0].type.name]}} className="pokemon-stat-view__background"></div>
+        }
+        return null;
     }
     
     render(){
         return(
             <div className="pokemon-stat-view">
-                <h1>{this.state.name}</h1>
-                <div className="pokemon-stat-view__stats"></div>
-                <div className="pokemon-stat-view__left"></div>
-                <div className="pokemon-stat-view__right"></div>
+                <div className="pokemon-stat-view__back">
+                     <i class="fas fa-arrow-left"></i>
+                </div>
+             
+                <div className="pokemon-stat-view__stats">
+                    <div className="pokemon-stat-view__stats__header">
+                         <h1 >{this.state.name}</h1>
+                         
+                    </div>
+                    <Stats image={this.state.image} types={this.state.types} colours={this.pokeColours} order={this.state.order} stats={this.state.stats} />
+                </div>
+                <this.renderBackgroundDivs />
+                
+
+                {/* <div className="pokemon-stat-view__left"></div>
+                <div className="pokemon-stat-view__right"></div> */}
             </div>
         )
     }
