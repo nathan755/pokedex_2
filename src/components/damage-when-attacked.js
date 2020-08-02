b6d40248c26d8c3ae9020aef5f4f77c72b9336ab
nathan <nathan@cubed.email>
2020-08-02 19:38:14 +0100

    trash yo

--- src/components/damage-when-attacked.js
+++ src/components/damage-when-attacked.js
@@ -3,133 +3,170 @@ import TypeBar from "./type-bar";
 import Axios from "axios";
 
 class DamageWhenAttacked extends Component {
-    constructor(props){
-        super(props)
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            typesData:[],
-            loading:true
-        }
+    this.state = {
+      typesData: [],
+      loading: true,
+    };
 
-        this.renderDamageBars = this.renderDamageBars.bind(this);
+    this.renderDamageBars = this.renderDamageBars.bind(this);
+  }
 
+  componentDidUpdate(prevProps) {
+    if (prevProps.types !== this.props.types) {
+      const requests = [];
+
+      this.props.types.forEach((type) => {
+        requests.push(
+          Axios.get(`https://pokeapi.co/api/v2/type/${type.type.name}`)
+        );
+      });
+
+      Promise.all(requests).then((res) => {
+        this.setState({
+          typesData: res,
+          loading: false,
+        });
+      });
     }
-    
-    componentDidUpdate(prevProps){
-        if(prevProps.types !== this.props.types){
-            
-            const requests = [];
-            
-            this.props.types.forEach( type => {
-                requests.push(Axios.get(`https://pokeapi.co/api/v2/type/${type.type.name}`))
-            });
-            
-            Promise.all(requests).then((res)=>{
-                this.setState({
-                    typesData:res,
-                    loading:false
-                })
-            })
-        }
-    }
+  }
 
-    renderDamageBars(){
+  renderDamageBars() {
+    if (!this.state.loading) {
+      // Pokemon can have up to to types. Below values are type ones damage multipliers.
+      const typeOneDoubleDamageValues = this.state.typesData[0].data.damage_relations.double_damage_from.map(
+        (type) => ({ name: type.name, multiplier: "2x" })
+      );
+      const typeOneHalfDamageValues = this.state.typesData[0].data.damage_relations.half_damage_from.map(
+        (type) => ({ name: type.name, multiplier: "0.5x" })
+      );
+      const typeOneNoDamageValues = this.state.typesData[0].data.damage_relations.no_damage_from.map(
+        (type) => ({ name: type.name, multiplier: "0x" })
+      );
+      const types = [
+        ...typeOneDoubleDamageValues,
+        ...typeOneHalfDamageValues,
+        ...typeOneNoDamageValues,
+      ];
 
-        if(!this.state.loading){
-
-            // Pokemon can have up to to types. Below values are type ones damage multipliers.
-            const typeOneDoubleDamageValues =  this.state.typesData[0].data.damage_relations.double_damage_from.map( type  => ({name:type.name,multiplier:"2x"}) )
-            const typeOneHalfDamageValues =  this.state.typesData[0].data.damage_relations.half_damage_from.map( type  => ({name:type.name,multiplier:"0.5x"}) )
-            const typeOneNoDamageValues = this.state.typesData[0].data.damage_relations.no_damage_from.map( type  => ({name:type.name,multiplier:"0x"}) )
-     
-            
-            if(this.state.typesData.length !== 1){
-                // If the pokemon has two types populate below arrays with their damage multipliers.
-                let typeTwoDoubleDamageValues =  this.state.typesData[1].data.damage_relations.double_damage_from.map( type  => ({name:type.name, multiplier:"2x"}) )
-                let typeTwoHalfDamageValues =  this.state.typesData[1].data.damage_relations.half_damage_from.map( type  =>  ({name:type.name, multiplier:"0.5x"}) )
-                let typeTwoNoDamageValues = this.state.typesData[1].data.damage_relations.no_damage_from.map( type  =>  ({name:type.name, multiplier:"0x"}) )
-                let  quadDamageValues = [];
-                
-                // If either type recieves no damage pokemon is immune.
-                const  filtered = typeOneNoDamageValues.filter(type=> !typeTwoNoDamageValues.includes(type.name));
-                const noDamageValues = [...filtered, ...typeTwoNoDamageValues];
-                // If type 1 and type 2 are both resistent i.e they receive only half damage, then the damage multiplier is 0.25.
-                const quarterDamageValues = []
-                console.log("typeOneHalfDamageValues",typeOneHalfDamageValues)
-                console.log("typeTwoHalfDamageValues",typeTwoHalfDamageValues)
-                typeOneHalfDamageValues.forEach((type) => {
-                    console.log("half")
-                    const inBoth = typeTwoHalfDamageValues.some(item => item.name === type.name)
-
-                   
-
-                    if(inBoth){
-                        console.log("hello")
-                        quarterDamageValues.push({name:type.name, multiplier:"0.25x"})
-                        
-                        const index = typeTwoHalfDamageValues.map(e => e.name).indexOf(type.name);
-                        console.log("index", index)
-                        typeTwoHalfDamageValues.splice(index,1)
-                    }
-                })
-               
-                
-                typeOneDoubleDamageValues.forEach((type)=>{
-                    const inBoth = typeTwoDoubleDamageValues.some(item => item.name === type.name)
-                    if(inBoth){
-                       
-                        quadDamageValues.push({name:type.name, multiplier:"4x"})
-                        
-                        const index = typeTwoDoubleDamageValues.map(e => e.name).indexOf(type.name);
-                   
-                        typeTwoDoubleDamageValues.splice(index,1)
-                    }
-                })
-                
-                const allTypeDamageMultipliers = [...quadDamageValues, ...typeTwoDoubleDamageValues, ...typeTwoHalfDamageValues, ...quarterDamageValues, ...noDamageValues]
-                
-
-                return (
-                    <div>
-                        {
-                            allTypeDamageMultipliers.map(type=><TypeBar type={type.name} multiplier={type.multiplier} colour={"#"+this.props.colours[type.name]} />)
-                        }
-                    </div>
-                )
-            }
-
-        
-
-
-
-            
-
- 
- 
- 
-            
-
-
-        }
-        
-        
-
-
-        return null;
-    }
-
-
-    render(){
-        
-        return(
-            <div className="damage-when-attacked">
-                
-                <this.renderDamageBars />
+      if (this.state.typesData.length === 1) {
+        return (
+          <div className="damage-when-attacked__bars-container">
+            <div className="flex-item">
+              {types.map((type) => (
+                <TypeBar
+                  type={type.name}
+                  multiplier={type.multiplier}
+                  colour={"#" + this.props.colours[type.name]}
+                />
+              ))}
             </div>
-        )
+          </div>
+        );
+      }
+
+      if (this.state.typesData.length !== 1) {
+        // If the pokemon has two types populate below arrays with their damage multipliers.
+        let typeTwoDoubleDamageValues = this.state.typesData[1].data.damage_relations.double_damage_from.map(
+          (type) => ({ name: type.name, multiplier: "2x" })
+        );
+        let typeTwoHalfDamageValues = this.state.typesData[1].data.damage_relations.half_damage_from.map(
+          (type) => ({ name: type.name, multiplier: "0.5x" })
+        );
+        let typeTwoNoDamageValues = this.state.typesData[1].data.damage_relations.no_damage_from.map(
+          (type) => ({ name: type.name, multiplier: "0x" })
+        );
+        let quadDamageValues = [];
+
+        // If either type recieves no damage pokemon is immune.
+        const filtered = typeOneNoDamageValues.filter(
+          (type) => !typeTwoNoDamageValues.includes(type.name)
+        );
+        const noDamageValues = [...filtered, ...typeTwoNoDamageValues];
+        // If type 1 and type 2 are both resistent i.e they receive only half damage, then the damage multiplier is 0.25.
+        const quarterDamageValues = [];
+        console.log("typeOneHalfDamageValues", typeOneHalfDamageValues);
+        console.log("typeTwoHalfDamageValues", typeTwoHalfDamageValues);
+        typeOneHalfDamageValues.forEach((type) => {
+          console.log("half");
+          const inBoth = typeTwoHalfDamageValues.some(
+            (item) => item.name === type.name
+          );
+
+          if (inBoth) {
+            console.log("hello");
+            quarterDamageValues.push({ name: type.name, multiplier: "0.25x" });
+
+            const index = typeTwoHalfDamageValues
+              .map((e) => e.name)
+              .indexOf(type.name);
+            console.log("index", index);
+            typeTwoHalfDamageValues.splice(index, 1);
+          }
+        });
+
+        typeOneDoubleDamageValues.forEach((type) => {
+          const inBoth = typeTwoDoubleDamageValues.some(
+            (item) => item.name === type.name
+          );
+          if (inBoth) {
+            quadDamageValues.push({ name: type.name, multiplier: "4x" });
+
+            const index = typeTwoDoubleDamageValues
+              .map((e) => e.name)
+              .indexOf(type.name);
+
+            typeTwoDoubleDamageValues.splice(index, 1);
+          }
+        });
+
+        const leftDamageMultipliers = [
+          ...quadDamageValues,
+          ...typeTwoDoubleDamageValues,
+          ...typeTwoHalfDamageValues,
+        ];
+
+        const rightDamageMultipliers = [
+          ...noDamageValues,
+          ...quarterDamageValues,
+        ];
+        return (
+          <div className="damage-when-attacked__bars-container">
+            <div className="flex-item">
+              {leftDamageMultipliers.map((type) => (
+                <TypeBar
+                  type={type.name}
+                  multiplier={type.multiplier}
+                  colour={"#" + this.props.colours[type.name]}
+                />
+              ))}
+            </div>
+            <div className="flex-item">
+              {rightDamageMultipliers.map((type) => (
+                <TypeBar
+                  type={type.name}
+                  multiplier={type.multiplier}
+                  colour={"#" + this.props.colours[type.name]}
+                />
+              ))}
+            </div>
+          </div>
+        );
+      }
     }
 
+    return null;
+  }
 
+  render() {
+    return (
+      <div className="damage-when-attacked">
+        <this.renderDamageBars />
+      </div>
+    );
+  }
 }
 
 export default DamageWhenAttacked;
